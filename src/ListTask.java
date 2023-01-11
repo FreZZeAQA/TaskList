bfd68bdafb5c80c5e2aee7fca15032ff9cb84b76
Yura <yuras92@ukr.net>
2023-01-12 00:04:53 +0200

    Add new Branch

---
+++ src/ListTask.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ListTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> wordList = new ArrayList<>();
+        String word;
+        do {
+            System.out.print("Word:");
+            word = scanner.nextLine().toLowerCase();
+            wordList.add(word);
+        } while (!word.equals("break"));
+        System.out.println("Default list:" + wordList);
+
+        System.out.print("Words startsWith < s >: ");
+        for (String wordStartsWithS : wordList) {
+            Pattern patternStarts = Pattern.compile("^s");
+            Matcher matcherS = patternStarts.matcher(wordStartsWithS);
+            while (matcherS.find()) {
+                System.out.print(wordStartsWithS + ", ");
+            }
+        }
+        System.out.print("\nWords with length more than five: ");
+        for (String wordsWithLengthMoreThanFive : wordList) {
+            Pattern patternLength = Pattern.compile("[a-zA-Z]{5}");
+            Matcher matcherLength = patternLength.matcher(wordsWithLengthMoreThanFive);
+            while (matcherLength.find()) {
+                System.out.print(wordsWithLengthMoreThanFive+", ");
+            }
+        }
+    }
+}
+
